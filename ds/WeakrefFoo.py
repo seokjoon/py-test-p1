f1b6f29b4ff51409541af6c3aee599adc02ae480
seokjoon <seokjoon@gmail.com>
2021-05-02 10:00:33 +0900

    py lib 3rd

---
+++ ds/WeakrefFoo.py
@@ -0,0 +1,47 @@
+import gc
+import weakref
+
+
+class WeakrefFoo:
+    def __del__(self):
+        print(self)
+
+
+def callback(ref):
+    print(ref)
+
+
+foo = WeakrefFoo()
+# bar = weakref.ref(foo)
+bar = weakref.ref(foo, callback)
+
+print(foo)
+print(bar)
+del foo
+print(bar)
+print()
+
+
+def on_finalize(*args):
+    print(args)
+
+
+foo = WeakrefFoo
+bar = weakref.finalize(foo, on_finalize, 'BAR')
+print(bar)
+del foo
+print(bar)
+print()
+
+
+
+foo = WeakrefFoo
+foo_id = id(foo)
+bar = weakref.finalize(foo, on_finalize, foo)
+bar.atexit = False
+print(bar)
+del foo
+print(bar)
+for o in gc.get_objects():
+    if id(o) == foo_id:
+        print('found uncollected object in gc')
